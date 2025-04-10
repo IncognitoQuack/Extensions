504f12955a5ca6afdbd34bc06625c88d03740073
IncognitoQuack <154409637+IncognitoQuack@users.noreply.github.com>
2025-04-11 00:48:35 +0530

    Delete background directory

--- background/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(function() {
-    console.log('ElabXCS extension installed');
-    
-    // Initialize storage with default values
-    chrome.storage.local.set({
-      editModeEnabled: false,
-      lastText: ''
-    });
-  });
